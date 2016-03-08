d34616d05e4d0f1636e024185d1e4b3987791b32
Bill Zerega <zerega85@gmail.com>
2016-03-08 16:42:30 -0500

    routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def sign_up
+
+  end
+
+  def sign_out
+
+  end
+
+  def logged_in
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get 'users/sign_up'
+  get 'users/sign_out'
+  get 'users/logged_in'
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
